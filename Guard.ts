da7a207e979d7f97784064a2ff6c16c11fc22379
Nick Hodges <nickhodges@gmail.com>
2020-03-15 11:53:26 -0400

    Added number range checking.

--- Guard.ts
+++ Guard.ts
@@ -19,6 +19,13 @@ class EArgumentIsNullOrUndefinedError extends EGuardError {
   }
 }
 
+class ENumberIsOutOfRange extends EGuardError {
+  constructor(m: string) {
+    super(m);
+    Object.setPrototypeOf(this, ENumberIsOutOfRange.prototype);
+  }
+}
+
 export class Guard {
   // Private Methods
   private static isDefined<T>(value: T | undefined | null): value is T {
@@ -31,6 +38,10 @@ export class Guard {
 
   private static raiseArgumentIsNullOrUndefinedError(aMessage: string) {
     throw new EArgumentIsNullOrUndefinedError(aMessage);
+  }
+
+  private static raiseNumberIsOutOfRangeError(aMessage: string) {
+    throw new ENumberIsOutOfRange(aMessage);
   }
 
   // Public Methods (all of them should be static)
@@ -51,4 +62,17 @@ export class Guard {
       Guard.raiseArgumentIsNullOrUndefinedError(aMessage);
     }
   }
+
+  public static thatNumbersAreInRangeInclusive(value: number, min: number, max: number, aMessage: string) {
+    if ((value < min) && (value > max)) {
+      Guard.raiseNumberIsOutOfRangeError(aMessage);
+    }
+  }
+
+  public static thatNumbersAreInRangeExclusive(value: number, min: number, max: number, aMessage: string) {
+    if ((value <= min) && (value >= max)) {
+      Guard.raiseNumberIsOutOfRangeError(aMessage);
+    }
+  }
+
 }
